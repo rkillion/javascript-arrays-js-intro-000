0b3722057a763b7df567c0b6b7c3a01b806009cd
Robert Killion <robertfkillion@gmail.com>
2021-05-06 21:38:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,5 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
+var addElementToBeginningOfArray = (array,element) => {
+  var newArray = [...array,element];
+  return newArray;
+};
